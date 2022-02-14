86c3474451c5db08bea79bd150f6f8f0a49ec6e0
Kevin <sxzz@sxzz.moe>
2022-02-15 07:31:48 +0800

    feat(docs): add source and contributors
    closes #6038

--- docs/.vitepress/vitepress/index.ts
+++ docs/.vitepress/vitepress/index.ts
@@ -17,6 +17,7 @@ import IconList from './components/globals/icons.vue'
 
 import ParallaxHome from './components/globals/parallax-home.vue'
 import Resource from './components/globals/resource.vue'
+import Contributors from './components/globals/contributors.vue'
 
 import type { Component } from 'vue'
 
@@ -32,4 +33,5 @@ export const globals: [string, Component][] = [
   ['Changelog', Changelog],
   ['ParallaxHome', ParallaxHome],
   ['Resource', Resource],
+  ['Contributors', Contributors],
 ]
